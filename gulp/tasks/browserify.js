fcb0a8d56a4b01c0a2ffb70bd45a52091117232c
SanCoder <sancoder.q@gmail.com>
2015-09-05 13:51:52 +0800

    generate source map for bundle.js

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -2,10 +2,13 @@ var gulp = require("gulp");
 var browserify = require("browserify");
 var source = require('vinyl-source-stream');
 var buffer = require('vinyl-buffer');
+var sourcemaps = require('gulp-sourcemaps');
+var uglify = require('gulp-uglify');
 
 var paths = {
 	source: __base + '/src/app.jsx',
-	javascript: __base + '/public/js/' 
+	javascript: __base + '/public/js/',
+    map: __base + '/public/js/bundle.js.map'
 };
 
 module.exports = function() {
@@ -17,6 +20,9 @@ module.exports = function() {
 		.transform(['reactify', { 'es6': true }, 'envify'])
 		.bundle()
 		.pipe(source('bundle.js'))
-		.pipe(buffer())
+        .pipe(buffer())
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(sourcemaps.write('./'))
 		.pipe(gulp.dest(paths.javascript));
 };
